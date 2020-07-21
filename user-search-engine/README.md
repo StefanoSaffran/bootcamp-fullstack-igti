d0171bf6ec3fad25eb3289af5bb8ef92452776a7
Stefano Saffran <stefanosaffran@hotmail.com>
2020-07-21 12:36:38 +0200

    feat: add user search project

---
+++ user-search-engine/README.md
@@ -0,0 +1,44 @@
+  <p align="center">
+  <img src="https://res.cloudinary.com/stefanosaffran/image/upload/v1594655952/igti/xrqczgfrqnugg8ztjueu.png" />
+</p>
+
+<h3 align="center">
+  Module 1 - Challenge - User Search Engine
+</h3>
+
+  <p align="center">
+<img alt="Made by Stefano" src="https://img.shields.io/badge/made%20by-StefanoSaffran-%20">
+</p>
+
+<p align="center">
+  <a href="#computer-project">Project</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#mailbox_with_mail-entre-em-contato">Get in touch!</a>
+</p>
+
+## :computer: Project 
+
+Application, using **HTML**, **CSS** and **Vanilla JavaScript**, to search and filter users and show statistics about the search results.
+
+> **Netlify:**
+> 👉 [User Search Engine](https://user-search-igti-bootcamp.netlify.app/)
+
+<p align="center">
+  <img src="https://res.cloudinary.com/stefanosaffran/image/upload/v1595325995/igti/desafios/c9dstnvipxz6ymjprtfx.gif" >
+</p>
+
+
+  ## :mailbox_with_mail: Get in touch!
+
+<a href="https://stefanosaffran.com" target="_blank" >
+  <img alt="Website - Stefano Saffran" src="https://img.shields.io/badge/Website--%23F8952D?style=social">
+</a>&nbsp;&nbsp;&nbsp;
+<a href="https://www.linkedin.com/in/stefanosaffran/" target="_blank" >
+  <img alt="Linkedin - Stefano Saffran" src="https://img.shields.io/badge/Linkedin--%23F8952D?style=social&logo=linkedin">
+</a>&nbsp;&nbsp;&nbsp;
+<a href="mailto:stefanoas@gmail.com" target="_blank" >
+  <img alt="Email - Stefano Saffran" src="https://img.shields.io/badge/Email--%23F8952D?style=social&logo=gmail">
+</a> 
+
+---
+
+Made with :coffee: and ❤️ by Stefano Saffran.
---
+++ user-search-engine/js/script.js
@@ -0,0 +1,192 @@
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+const usersTitle = document.querySelector('.users h2');
+
+let tabStatistics = null;
+let tabUsers = null;
+let container = null;
+
+let allUsers = [];
+let filteredUsers = []
+let statistics = {
+  men: 0,
+  women: 0,
+  agesSum: 0,
+  averageAge: 0.0,
+}
+
+window.addEventListener('load', () => {
+  tabStatistics = document.querySelector('.statistics');
+  tabUsers = document.querySelector('.users');
+  container = document.querySelector('.container');
+
+
+  numberFormat = Intl.NumberFormat('pt-BR');
+
+  fetchUsers();
+
+  input.addEventListener("keyup", event => { 
+    event.preventDefault();
+  
+    if (input.value === '') {
+      button.disabled = true;
+      return
+    }
+
+    button.disabled = false;
+  })
+  
+  document.querySelector("form").addEventListener("submit", handleSubmit);
+});
+
+async function fetchUsers() {
+  setLoading();
+
+  setTimeout(async () => {
+    const response = await fetch('https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo');
+    const { results } = await response.json();
+    setLoading(false);
+
+    allUsers = results.map(user => {
+      const { name: { first, last }, gender, picture: { medium }, dob: { age } } = user;
+    
+      return {
+        name: `${first} ${last}`,
+        gender,
+        picture: medium,
+        age,
+      }
+    })
+  }, 2000);
+}
+
+const setLoading = (loading = true) => {
+  if (loading) {
+    input.disabled = true;
+    let loadingEl = document.createElement('div');
+    loadingEl.classList.add('col-sm-2');
+    loadingEl.setAttribute('id', 'loading');
+    loadingEl.innerHTML = `
+      <div class="sp sp-wave"></div>
+    `
+    container.appendChild(loadingEl);
+  } else {
+    document.querySelector('#loading').remove();
+    input.disabled = false;
+    input.focus();
+  }
+}
+
+const handleSubmit = async event => {
+  event.preventDefault();
+
+  filteredUsers = [];
+  const { value } = input;
+  const regex = new RegExp(removeAccents(value), 'i');
+
+  filteredUsers = allUsers.filter(user => regex.test(removeAccents(user.name)));
+  
+  if (!filteredUsers.length) {
+    tabUsers.style.marginTop = '60px';
+  } else {
+    tabUsers.style.marginTop = '30px';
+  }
+
+  filteredUsers.sort((a, b) => {
+    return a.name.localeCompare(b.name);
+  });
+
+  render();
+}
+
+const render = () => {
+  renderUsers();
+  renderStatistics();
+}
+
+const renderUsers = () => {
+  tabUsers.innerHTML = '';
+  let usersHTML = '<div class="users-container">';
+  
+  if(filteredUsers.length) {
+    usersTitle.textContent = `Found ${filteredUsers.length} users`
+    filteredUsers.forEach(user => {
+      const {name, picture, age } = user;
+  
+      let userHTML = `
+        <div class="user">
+          <img src="${picture}" alt="${name}">
+          <div>
+            <span><strong>${name}</strong>, ${age} anos</span>
+          </div>
+        </div>
+      `;
+
+      usersHTML += userHTML;
+    });
+
+    usersHTML += '</div>';
+    tabUsers.appendChild(usersTitle);
+    tabUsers.innerHTML += usersHTML;
+    tabStatistics.classList.remove('hide');
+  } else {
+    usersTitle.textContent = `No results for: '${input.value}'`
+    tabUsers.style.marginTop = '60px';
+    tabUsers.appendChild(usersTitle);
+    tabStatistics.classList.add('hide');
+
+  }
+}
+
+const renderStatistics = () => {
+  tabStatistics.innerHTML = '';
+
+  if (!filteredUsers.length) {
+    statistics = {
+      men: 0,
+      women: 0,
+      agesSum: 0,
+      averageAge: 0.0,
+    }
+
+    return;
+  }
+  
+  statistics.men = filteredUsers.filter(user => user.gender === 'male').length;
+  statistics.women = filteredUsers.filter(user => user.gender === 'female').length;
+  statistics.agesSum = filteredUsers.reduce((acc, nextUser) => acc += nextUser.age, 0);
+  statistics.averageAge = (statistics.agesSum / filteredUsers.length).toFixed(2);
+
+  tabStatistics.innerHTML += `
+    <h2>Statistics</h2>
+    <div>
+      <span><strong>Men: </strong>${statistics.men}</span>
+      <span><strong>Women: </strong>${statistics.women}</span>
+      <span><strong>Ages sum: </strong>${statistics.agesSum}</span>
+      <span><strong>Average age: </strong>${statistics.averageAge}</span>
+    </div>
+  ` 
+}
+
+const removeAccents = newStringWithAccent => {
+  let string = newStringWithAccent;
+  
+	let mapAccentsHex 	= {
+		a : /[\xE0-\xE6]/g,
+		e : /[\xE8-\xEB]/g,
+		i : /[\xEC-\xEF]/g,
+		o : /[\xF2-\xF6]/g,
+		u : /[\xF9-\xFC]/g,
+		c : /\xE7/g,
+		n : /\xF1/g
+	};
+
+	for ( let letter in mapAccentsHex ) {
+		let regex = mapAccentsHex[letter];
+		string = string.replace( regex, letter );
+  }
+
+	return string;
+}
+
